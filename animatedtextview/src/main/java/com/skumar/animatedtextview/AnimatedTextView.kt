dbfcb8e1ea4c4d8e3ecf9e33e162e7d80db56c26
ksughosh <k.sughosh@gmail.com>
2017-07-25 16:09:32 +0200

    - changed to AppCompatTextView from TextView

--- animatedtextview/src/main/java/com/skumar/animatedtextview/AnimatedTextView.kt
+++ animatedtextview/src/main/java/com/skumar/animatedtextview/AnimatedTextView.kt
@@ -4,7 +4,6 @@ import android.animation.*
 import android.content.Context
 import android.content.res.TypedArray
 import android.support.annotation.StringRes
-import android.support.v4.widget.AutoSizeableTextView
 import android.support.v7.widget.AppCompatTextView
 import android.util.AttributeSet
 import android.util.TypedValue
